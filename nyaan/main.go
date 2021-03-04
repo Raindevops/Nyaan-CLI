0644b2ea79ac10e6174b6f4bb39653f502fb657d
raindevops <raindevops.durand@gmail.com>
2021-03-04 09:31:56 +0100

    init route table, and check command run

--- nyaan/main.go
+++ nyaan/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Helper command is called")
+		return
+	}
+
 	fmt.Println("Entrypoint")
 }
--- nyaan/route/table.go
+++ nyaan/route/table.go
@@ -1,5 +1,8 @@
 package route
 
+// Args : List of available arguments from the command line
+var Args = [3]string{"create", "update", "delete"}
+
 func table() {
 
 }
